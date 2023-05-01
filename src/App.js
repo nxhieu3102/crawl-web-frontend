84455c4ca0291baa76a7087c4ff2f02df0c4b8b3
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-05-01 19:20:41 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -4,31 +4,31 @@ import Home from "./Pages/Home"
 import Search from "./Pages/Search"
 import Product from "./Pages/Product"
 const App = () => {
-    const routes = [
-        { path: '/', element: <Home /> },
-        { path: '/search', element: <Search /> },
-        { path: '/product', element: <Product /> }
+	const routes = [
+		{ path: '/', element: <Home /> },
+		{ path: '/search', element: <Search /> },
+		{ path: '/product', element: <Product /> }
 
-    ]
+	]
 
-    return (
-        <BrowserRouter>
-        <Routes>
-          {routes.map((route, index) => {
-            return (
-              <Route key={index}
-                path={route.path}
-                element={
-                  <Layout>
-                    {route.element}
-                  </Layout>
-                }
-              />
-            );
-          })}
-        </Routes>
-      </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Routes>
+				{routes.map((route, index) => {
+					return (
+						<Route key={index}
+							path={route.path}
+							element={
+								<Layout>
+									{route.element}
+								</Layout>
+							}
+						/>
+					);
+				})}
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
