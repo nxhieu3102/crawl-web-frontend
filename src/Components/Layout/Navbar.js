fc8bc86bd24ef79f1da884fb77f9c2c6a3d3be08
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-05-01 19:20:47 +0700

    navbar

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -5,18 +5,19 @@ const NavBar = () => {
     return (
         <div className='bg-[#515154] flex justify-center'>
             <div className="w-full max-w-[1200px] h-[50px] bg-[#515154] top-0 flex justify-between items-center">
-                <Link  to="/" className='mx-[8px] cursor-pointer'>
+                <Link to="/" className='mx-[8px] cursor-pointer'>
                     <img src={LOGO} alt="logo" className='h-[40px]' />
                 </Link>
                 <div className='w-[100px] text-[14px] h-full text-white mx-[8px] flex justify-center items-center hover:bg-[#ccc] cursor-pointer'>
-                    <Link to=".">Điện thoại</Link>
+                    <Link to="/search"
+                        state={{ typeProduct: "dienthoai" }}>Điện thoại</Link>
                 </div>
                 <div className='w-[100px] text-[14px] h-full text-white mx-[8px] flex justify-center items-center hover:bg-[#ccc] cursor-pointer'>
-                    <Link to=".">Laptop</Link>
+                    <Link to="/search" state={{ typeProduct: "laptop" }}>Laptop</Link>
 
                 </div>
                 <div className='w-[100px] text-[14px] h-full text-white mx-[8px] flex justify-center items-center hover:bg-[#ccc] cursor-pointer'>
-                    <Link to=".">Tablet</Link>
+                    <Link to="/search" state={{ typeProduct: "tablet" }}>Tablet</Link>
                 </div>
                 <Link to="/search" className='w-[100px] text-[14px] h-full text-white mx-[8px] flex justify-center items-center hover:bg-[#ccc] cursor-pointer'>Tìm kiếm</Link>
             </div>
